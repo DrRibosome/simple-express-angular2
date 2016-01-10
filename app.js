2345729c7af9ea89da875b63bd9a475d9f35da26
Jack Crawford <jdc2172@gmail.com>
2016-01-10 10:50:29 -0500

    cleaning

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
 
-//app.use(express.static('node_modules'));
-//app.set('views', path.join(__dirname, 'views'));
-
 app.use('/modules', express.static(__dirname + '/node_modules/'));
 app.use('/app', express.static(__dirname + '/app/'));
 app.use('/', express.static(__dirname + '/views/'));
